0206fb72775095ff1da58eb9a081ebd1a5202f90
evgeniakovako <ainutlaatuinen1@gmail.com>
2026-06-22 16:56:00 +0700

    Add SELECT examples

---
+++ select.sql
@@ -0,0 +1,17 @@
+-- ==========================================
+-- File: select.sql
+-- Topic: SELECT
+-- Author: Evgeniya Kovako
+-- ==========================================
+
+-- Task 1
+-- Display all columns from the users table.
+
+SELECT *
+FROM users;
+
+-- Task 2
+-- Display only the names of users.
+
+SELECT name
+FROM users;
